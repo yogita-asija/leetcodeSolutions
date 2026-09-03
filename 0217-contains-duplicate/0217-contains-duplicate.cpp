83f0d8b3ecd1a40bb27af3f903dc5b67bfe39bde
Yogita Asija <yogitaasija1@gmail.com>
2026-09-03 21:49:46 +0530

    Time: 78 ms (31.02%), Space: 111.3 MB (46.25%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for (int num: nums){
+            if (st.find(num)!=st.end()){
+                return true;
+            }
+            st.insert(num);
+        }
+     return false;    
+        
+    }
+   
+};
