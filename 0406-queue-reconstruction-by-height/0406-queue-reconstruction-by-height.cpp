e8472a97b6b34422ef9003b8cdfb6b5061d905f3
Yogita Asija <yogitaasija1@gmail.com>
2026-09-19 23:02:23 +0530

    Time: 16 ms (76.5%), Space: 16 MB (64.49%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), [](vector<int>& a, vector<int>& b) {
+              if (a[0] == b[0])
+                return a[1] < b[1];
+
+            return a[0] > b[0];
+        });
+        vector<vector<int>> ans;
+
+        // Insert person at index k
+        for (auto& person : people) {
+            ans.insert(ans.begin() + person[1], person);
+        }
+
+        return ans;
+    }
+
+
+
+
+        
+        
+    
+};
